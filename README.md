c4ab2f45e68416b94b26c8988eccef0846db3f79
Sami Barghshoon <chugah@users.noreply.github.com>
2017-05-05 14:18:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# d3-react-bar-chart
+
+React and d3 bar chart with animation.
+
+# To start
+
+Clone project
+
+```
+$ npm install
+$ npm start
+```
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import BarChart from './components/BarChart';
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <BarChart />
+      </div>
+    );
+  }
+}
+
+
+
